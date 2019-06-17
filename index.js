06a567f5611a987f16d39f5f9f897380dcd9c4f1
golisitaram007 <sitaram007@outlook.com>
2019-06-17 21:59:24 +0800

    Adding html and procfile for deploying

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const http = require("http");
 const app = require("./app");
+const path = require('path');
 
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 server.listen(port);
